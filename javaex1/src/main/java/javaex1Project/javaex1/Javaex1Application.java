9035bbcd621a62c1d739257ea74571087e92745c
jungseokcha <wjdtjr9401@gmail.com>
2023-01-04 19:42:06 +0900

    #1 환경설정 및 spring boot를 이용하여 프로젝트 생성

---
+++ javaex1/src/main/java/javaex1Project/javaex1/Javaex1Application.java
@@ -0,0 +1,13 @@
+package javaex1Project.javaex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaex1Application.class, args);
+	}
+
+}
---
+++ javaex1/src/test/java/javaex1Project/javaex1/Javaex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package javaex1Project.javaex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
